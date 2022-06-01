37eb511764b7e91fcfdaecb39c64512a206fbbcf
lismanarsilo <lismanarsilo@gmail.com>
2022-06-01 19:29:41 +0700

    Mengubah quiz_3 mencari maxnumber menggunakan perulangan forEach

--- quiz_3/01-MaxNumber.js
+++ quiz_3/01-MaxNumber.js
@@ -3,10 +3,16 @@ const numbers = [44, 131, 335, 223, 21, 66, 87];
 
 function maxNumber(arrays) {
   // untuk mengecek apakah array berupa number atau bukan jika iya maka kembalikan nilai This syntax is not allowed. The result is NaN.
+  var max = 0;
   if (isNaN(...arrays)) {
     return `This syntax is not allowed. The result is NaN.`;
+  } else {
+    arrays.forEach((el) => {
+      if (el > max) {
+        max = el;
+      }
+    });
   }
-  const max = Math.max.apply(null, arrays);
   return max;
 }
 
